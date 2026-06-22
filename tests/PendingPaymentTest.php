c3e4e674581fe0a9a534e5da362f16801bfb91d4
sunaoka <sunaoka@pocari.org>
2026-06-22 11:28:11 +0900

    comment out debug output statements

--- tests/PendingPaymentTest.php
+++ tests/PendingPaymentTest.php
@@ -53,7 +53,7 @@ final class PendingPaymentTest extends TestBoilerplate
         $this->data=$paymentDetails["data"];
         $this->assertEquals('SUCCESS', $paymentDetails['resultInfo']['code']);
 
-        echo'\n===================Pending Payment Details===================\n';
+        // echo'\n===================Pending Payment Details===================\n';
     }
     /**
      * Cancel pending payment
